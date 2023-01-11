cd821afe92ea59222ddedf2a3ee29a1a0e6d9d7a
zaidkhan0997 <kzaid0997@gmail.com>
2023-01-11 20:28:40 +0530

    sweet: Switch to vantom and playground 17 clang
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+# clone
+git clone -b 17 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
+git clone -b courbet-13 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
