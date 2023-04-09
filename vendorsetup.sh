16f710f5a851737a64c0d2b55eec48a420d0d861
zaidkhan0997 <kzaid0997@gmail.com>
2023-04-10 01:45:53 +0530

    sweet: Switch to Aghisna kernel and cosmic clang
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+# clone
+git clone -b master --depth=1 https://gitlab.com/GhostMaster69-dev/cosmic-clang.git prebuilts/clang/host/linux-x86/clang-cosmic
+git clone -b Aghisna-oss --depth=1 https://github.com/RooGhz720/Aghisna_Sweet_Kernel.git kernel/xiaomi/sm6150
